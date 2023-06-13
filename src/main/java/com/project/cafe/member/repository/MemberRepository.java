e6edafc6e2f86e0c1bdbca2c6999b92f6b5cc0da
임혜지 <ihj028003@gmail.com>
2023-06-14 00:27:25 +0900

    add : memberVO 생성

---
+++ src/main/java/com/project/cafe/member/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.project.cafe.member.repository;
+
+import com.project.cafe.member.vo.MemberVO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<MemberVO,String> {
+    //이름 규칙을 따라 메소드를 커스텀할 경우 선언
+}
---
+++ src/main/java/com/project/cafe/member/vo/MemberVO.java
@@ -0,0 +1,99 @@
+package com.project.cafe.member.vo;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//MySQL 테이블 삭제!
+//유효성 검사(null 어노테이션)
+/*
+외래키일 경우 @OneToOne, ...
+ */
+@Entity(name="member")
+@DynamicInsert //insert 시에 null이 아닌 값만 적용되므로, null이 들어온 경우 명시된 기본값 적용
+//@Table(name="member")
+public class MemberVO {
+
+    @Id
+    @Column(name="memberId", nullable = false, length = 20)
+    private String memberId;
+
+    @Column(name="grade", nullable = false, length = 20)
+    private String grade;
+
+    @Column(name="memberPwd", nullable = false, length = 500)
+    private String memberPwd;
+
+    @Column(name="memberName", nullable = false, length = 20)
+    private String memberName;
+
+    @Column(name="memberEmail", nullable = false, length = 40)
+    private String memberEmail;
+
+    @Column(name="loginCnt")
+    @ColumnDefault("0")
+    private int loginCnt;
+
+    public MemberVO() {}
+
+    public MemberVO(String memberId, String grade, String memberPwd, String memberName, String memberEmail, int loginCnt) {
+        setMemberId(memberId);
+        setGrade(grade);
+        setMemberPwd(memberPwd);
+        setMemberName(memberName);
+        setMemberEmail(memberEmail);
+        setLoginCnt(loginCnt);
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getMemberPwd() {
+        return memberPwd;
+    }
+
+    public void setMemberPwd(String memberPwd) {
+        this.memberPwd = memberPwd;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    public int getLoginCnt() {
+        return loginCnt;
+    }
+
+    public void setLoginCnt(int loginCnt) {
+        this.loginCnt = loginCnt;
+    }
+}
