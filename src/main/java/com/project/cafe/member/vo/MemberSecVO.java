9a8d61e8be6733b4bb6c23e8cf6ac21e8c8c526a
임혜지 <ihj028003@gmail.com>
2023-06-14 21:30:23 +0900

    add : memberSecVO 생성

---
+++ src/main/java/com/project/cafe/member/vo/MemberSecVO.java
@@ -0,0 +1,41 @@
+package com.project.cafe.member.vo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+//유효성 검사(null 어노테이션)
+@Entity(name="memberSec")
+public class MemberSecVO {
+
+    @Id
+    @Column(name="memberId", nullable = false, length = 20)
+    private String memberId;
+
+    @Column(name="salt", nullable = false, length = 500)
+    private String salt;
+
+    public MemberSecVO() {
+    }
+
+    public MemberSecVO(String memberId, String salt) {
+        setMemberId(memberId);
+        setSalt(salt);
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+}
